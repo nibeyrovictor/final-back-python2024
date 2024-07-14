be84402a5d40d1f948a5eb23b489307c2da316de
nibeyrovictor <nibeyrovictor@outlook.com.ar>
2024-07-13 23:09:47 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# final-back-python2024
---
+++ app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    # return "<h1>Hello, World!</h1>"
+    return render_template("index.html")
+
