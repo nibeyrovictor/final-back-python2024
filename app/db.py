7928a177cb77863e072d493e4ffe54d637ceca8d
nibeyrovictor <nibeyrovictor@outlook.com.ar>
2024-07-14 19:39:45 -0300

    FOR DEPLOY

--- app/db.py
+++ app/db.py
@@ -3,10 +3,10 @@ import mysql.connector
 # Conexion a la base de datos
 def conectarMysql():
     conexion = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='argentur'
+        host='Nibpos.mysql.pythonanywhere-services.comt',
+        user='Nibpos',
+        password='Nyp-2020DG5415cac',
+        database='Nibpos$argentSDFTur'
     )
     return conexion
 
